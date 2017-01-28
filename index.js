1b6fa8c6d954daae8dda8437d73e02485f1a89aa
Dmitry Nizovsky <dmitry.nizovsky@gmail.com>
2017-01-28 20:23:27 +0600

    remvoe ff class in new file and add merge config

--- index.js
+++ index.js
@@ -1,62 +1,38 @@
-var path        = require("path");
-var loaderUtils = require("loader-utils");
-
-function FeatureFlag(config) {
-  this.config = config;
-}
-
-FeatureFlag.prototype.getModule = function(name) {
-  for (var i = 0, max = this.config.length; i < max; i++) {
-    if (this.config[i].state) {
-      var modules = this.config[i].modules;
-
-      if (modules[name]) {
-        return modules[name];
-      }
-    }
-  }
-
-  return name;
-};
-
-function parseConfigPath(config) {
-  for (var i = 0, max = config.length; i < max; i++) {
-    var modules = {};
-    for (var k in config[i].modules) {
-      if (!config[i].modules.hasOwnProperty(k)) continue;
-      modules[resolve(k)] = resolve(config[i].modules[k]);
-    }
-
-    config[i].modules = modules;
-  }
-
-  return config;
-}
-
-function resolve(name) {
-  return require.resolve(path.resolve(name));
-}
+var utils       = require("loader-utils");
+var FeatureFlag = require("./lib/feature-flag");
 
 module.exports = function() {};
 
-module.exports.pitch = function(remainingRequest, precedingRequest, data) {
-  var query        = loaderUtils.parseQuery(this.query);
-  var configPath   = query.config || false;
-  var modulePathes = remainingRequest.split('!');
-  var modulePath   = modulePathes.length > 1 ? modulePathes[modulePathes.length-1] : remainingRequest;
-  var currentPath  = modulePath;
+module.exports.pitch = function (remainingRequest) {
+    var query      = utils.parseQuery(this.query);
+    var configPath = query.config || false;
+    var rr = remainingRequest.split('!');
+    var currentPath = rr[rr.length - 1];
+    var newPath = currentPath;
 
-  if (configPath) {
-    config = require(resolve(configPath));
-    config = parseConfigPath(config);
+    if (configPath) {
+        var ff = new FeatureFlag();
+        config = require(ff.resolve(configPath));
 
-    var ff = new FeatureFlag(config);
-    currentPath = ff.getModule(modulePath);
+        ff.setConfig(config);
+        newPath = ff.getModule(currentPath);
 
-    if (modulePathes.length > 1) {
-      currentPath = remainingRequest.replace(modulePath, currentPath);
+        if (Array.isArray(newPath)) {
+            try {
+                var data = require(currentPath);
+
+                for (var i = 0, max = newPath.length; i < max; i++) {
+                    data = Object.assign(data, require(newPath[i]));
+                }
+            } catch (e) {
+                throw "The merger is not possible check include files if they return data is not an object [" + currentPath + ", " + newPath.join(', ') + "]";
+            }
+
+            return "module.exports = " + JSON.stringify(data) + ";";
+        }
+
+        newPath = remainingRequest.replace(currentPath, newPath);
     }
-  }
 
-  return "module.exports = require("+loaderUtils.stringifyRequest(this, "!!" + currentPath)+");";
+    return "module.exports = require(" + utils.stringifyRequest(this, "!!" + newPath) + ");";
 };
