a74334fc14491504ed38b0e94c03a8f286000809
Dmitry Nizovsky <dnizovsky@thumbtack.net>
2017-02-15 12:25:59 +0600

    Add license and support transfer config with ff-plugin

--- index.js
+++ index.js
@@ -1,38 +1,42 @@
 var utils       = require("loader-utils");
 var FeatureFlag = require("./lib/feature-flag");
 
+function isObject(o) {
+  return typeof o === 'object';
+}
+
 module.exports = function() {};
 
 module.exports.pitch = function (remainingRequest) {
-    var query      = utils.parseQuery(this.query);
-    var configPath = query.config || false;
-    var rr = remainingRequest.split('!');
-    var currentPath = rr[rr.length - 1];
-    var newPath = currentPath;
+  var query       = utils.parseQuery(this.query);
+  var configPath  = query.config || global.FFLoaderConfig || false;
+  var rr          = remainingRequest.split('!');
+  var currentPath = rr[rr.length - 1];
+  var newPath     = currentPath;
 
-    if (configPath) {
-        var ff = new FeatureFlag();
-        var config = require(ff.resolve(configPath));
+  if (configPath) {
+    var ff     = new FeatureFlag();
+    var config = isObject(configPath) ? configPath : require(ff.resolve(configPath));
 
-        ff.setConfig(config);
-        newPath = ff.getModule(currentPath);
+    ff.setConfig(config);
+    newPath = ff.getModule(currentPath);
 
-        if (Array.isArray(newPath)) {
-            try {
-                var data = require(currentPath);
+    if (Array.isArray(newPath)) {
+      try {
+        var data = require(currentPath);
 
-                for (var i = 0, max = newPath.length; i < max; i++) {
-                    data = Object.assign(data, require(newPath[i]));
-                }
-            } catch (e) {
-                throw "The merger is not possible check include files if they return data is not an object [" + currentPath + ", " + newPath.join(', ') + "]";
-            }
-
-            return "module.exports = " + JSON.stringify(data) + ";";
+        for (var i = 0, max = newPath.length; i < max; i++) {
+          data = Object.assign(data, require(newPath[i]));
         }
+      } catch (e) {
+        throw "The merger is not possible check include files if they return data is not an object [" + currentPath + ", " + newPath.join(', ') + "]";
+      }
 
-        newPath = remainingRequest.replace(currentPath, newPath);
+      return "module.exports = " + JSON.stringify(data) + ";";
     }
 
-    return "module.exports = require(" + utils.stringifyRequest(this, "!!" + newPath) + ");";
+    newPath = remainingRequest.replace(currentPath, newPath);
+  }
+
+  return "module.exports = require(" + utils.stringifyRequest(this, "!!" + newPath) + ");";
 };
